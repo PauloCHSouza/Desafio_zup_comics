a90899e3b119fcd6e414ad9ce14de7abd9092458
PauloCHSouza <paulo.charamba@gmail.com>
2021-07-04 16:43:52 -0300

    Update Comic.java

--- src/main/java/com/paulo_comics/comics/domain/Comic.java
+++ src/main/java/com/paulo_comics/comics/domain/Comic.java
@@ -3,21 +3,66 @@ package com.paulo_comics.comics.domain;
 import javax.persistence.*;
 
 @Entity(name = "comics")
+@Table(name = "comics")
 public class Comic {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(
+            name = "comicId",
+            updatable = false
+    )
 	private Long comicId;
-	private String titulo, descricao, autores;
+	
+	@Column(
+            name = "usuarioId",
+            updatable = false,
+            columnDefinition = "int"
+    )
+	private Long usuarioId;
+	
+	@Column(
+            name = "titulo",
+            nullable = false,
+            columnDefinition = "varchar(100)"
+    )
+	private String titulo;
+	
+	@Column(
+            name = "descricao",
+            nullable = false,
+            columnDefinition = "longtext"
+    )
+	private String descricao;
+	
+	@Column(
+            name = "autores",
+            nullable = false,
+            columnDefinition = "varchar(100)"
+    )
+	private String autores;
+	
+	@Column(
+            name = "preco",
+            nullable = false,
+            columnDefinition = "decimal"
+    )
 	private Double preco;
+	
+	@Column(
+            name = "isbn",
+            nullable = false,
+            columnDefinition = "int"
+    )
 	private Long isbn;
 	
 	public Comic() {
 		
 	}
 	 
-	public Comic(Long comicId, String titulo, Double preco, String autores, Long isbn, String descricao) {
+	public Comic(Long comicId, Long usuarioId, String titulo, Double preco, String autores, Long isbn, String descricao) {
 		this.comicId = comicId;
+		this.usuarioId = usuarioId;
 		this.titulo = titulo;
 		this.preco = preco;
 		this.autores = autores;
